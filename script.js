4e9c4a88cb506484e43e8ad2318ffe2adbef890a
Shan Naziripour <shan@naziripour.com>
2022-12-12 00:44:31 +0000

    button fixes

--- script.js
+++ script.js
@@ -405,10 +405,10 @@ app.component("figure-caption", {
   template: `<figcaption>
       <div class="form" v-if="editMode">
         <textarea ref="body" v-model="value" placeholder="Your Caption" />
-        <a class="button" v-on:click="send" :disabled="value === null" v-bind:href="link" target="_blank" title="Send your caption to me!">
+        <a class="button" v-on:click="send" v-bind:href="link" target="_blank" title="Send your caption to me!">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg>
         <span>Send</span></a>
-        <button class="nav" type="button" v-on:click="cancel" >
+        <button class="button" type="button" v-on:click="cancel" >
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M2.5 2v6h6M2.66 15.57a10 10 0 1 0 .57-8.38"/></svg>
         <span>Cancel</span></button>
       </div>
